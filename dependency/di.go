c738b846625e5dcaf4ff31624c44f639a172b380
krdelva <kevin.r.delva@gmail.com>
2025-05-14 00:24:06 -0400

    add subdirectory dependency in which I worked with dpendency injection, using existing interfaces in a different context

---
+++ dependency/di.go
@@ -0,0 +1,15 @@
+package dependency
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "Marley")
+}
